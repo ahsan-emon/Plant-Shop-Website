e167006a906d36e6cd058c1e5932843b17f412e6
ahsan-emon <ahsan.emon23@gmail.com>
2022-02-18 01:11:48 +0600

    javascript added. img zoom plug in added.
    Product item page update

---
+++ js/script.js
@@ -0,0 +1,107 @@
+// plant 1 
+document.getElementById('plant-1').addEventListener('click', function(){
+    const productImg = document.getElementById('product-img');
+    productImg.src = '../images/product-11.jpg';
+    const productName = document.getElementById('product-name');
+    const plantName = document.getElementById('plant-1-name');
+    productName.innerText = plantName.innerText;
+    const productPrice = document.getElementById('product-price');
+    const plantPrice = document.getElementById('plant-1-price');
+    productPrice.innerText = plantPrice.innerText;
+});
+
+// plant 2 
+document.getElementById('plant-2').addEventListener('click', function(){
+    const productImg = document.getElementById('product-img');
+    productImg.src = '../images/product-22.jpg';
+    const productName = document.getElementById('product-name');
+    const plantName = document.getElementById('plant-2-name');
+    productName.innerText = plantName.innerText;
+    const productPrice = document.getElementById('product-price');
+    const plantPrice = document.getElementById('plant-2-price');
+    productPrice.innerText = plantPrice.innerText;
+});
+
+// plant 3 
+document.getElementById('plant-3').addEventListener('click', function(){
+    const productImg = document.getElementById('product-img');
+    productImg.src = '../images/product-33.jpg';
+    const productName = document.getElementById('product-name');
+    const plantName = document.getElementById('plant-3-name');
+    productName.innerText = plantName.innerText;
+    const productPrice = document.getElementById('product-price');
+    const plantPrice = document.getElementById('plant-3-price');
+    productPrice.innerText = plantPrice.innerText;
+});
+
+// plant 4 
+document.getElementById('plant-4').addEventListener('click', function(){
+    const productImg = document.getElementById('product-img');
+    productImg.src = '../images/product-44.jpg';
+    const productName = document.getElementById('product-name');
+    const plantName = document.getElementById('plant-4-name');
+    productName.innerText = plantName.innerText;
+    const productPrice = document.getElementById('product-price');
+    const plantPrice = document.getElementById('plant-4-price');
+    productPrice.innerText = plantPrice.innerText;
+});
+
+// plant 5 
+document.getElementById('plant-5').addEventListener('click', function(){
+    const productImg = document.getElementById('product-img');
+    productImg.src = '../images/product-55.jpg';
+    const productName = document.getElementById('product-name');
+    const plantName = document.getElementById('plant-5-name');
+    productName.innerText = plantName.innerText;
+    const productPrice = document.getElementById('product-price');
+    const plantPrice = document.getElementById('plant-5-price');
+    productPrice.innerText = plantPrice.innerText;
+});
+
+// plant 6 
+document.getElementById('plant-6').addEventListener('click', function(){
+    const productImg = document.getElementById('product-img');
+    productImg.src = '../images/product-66.jpg';
+    const productName = document.getElementById('product-name');
+    const plantName = document.getElementById('plant-6-name');
+    productName.innerText = plantName.innerText;
+    const productPrice = document.getElementById('product-price');
+    const plantPrice = document.getElementById('plant-6-price');
+    productPrice.innerText = plantPrice.innerText;
+});
+
+// plant 7
+document.getElementById('plant-7').addEventListener('click', function(){
+    const productImg = document.getElementById('product-img');
+    productImg.src = '../images/product-77.jpg';
+    const productName = document.getElementById('product-name');
+    const plantName = document.getElementById('plant-7-name');
+    productName.innerText = plantName.innerText;
+    const productPrice = document.getElementById('product-price');
+    const plantPrice = document.getElementById('plant-7-price');
+    productPrice.innerText = plantPrice.innerText;
+});
+
+// plant 8 
+document.getElementById('plant-8').addEventListener('click', function(){
+    const productImg = document.getElementById('product-img');
+    productImg.src = '../images/product-88.jpg';
+    const productName = document.getElementById('product-name');
+    const plantName = document.getElementById('plant-8-name');
+    productName.innerText = plantName.innerText;
+    const productPrice = document.getElementById('product-price');
+    const plantPrice = document.getElementById('plant-8-price');
+    productPrice.innerText = plantPrice.innerText;
+});
+
+// plant 9
+document.getElementById('plant-9').addEventListener('click', function(){
+    const productImg = document.getElementById('product-img');
+    productImg.src = '../images/product-99.jpg';
+    const productName = document.getElementById('product-name');
+    const plantName = document.getElementById('plant-9-name');
+    productName.innerText = plantName.innerText;
+    const productPrice = document.getElementById('product-price');
+    const plantPrice = document.getElementById('plant-9-price');
+    productPrice.innerText = plantPrice.innerText;
+});
